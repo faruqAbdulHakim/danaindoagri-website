4df9bc7f44c51f26d6dd0fa3cc5ff267673e38e1
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2022-04-06 13:16:04 +0700

    project init

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+    <main className='min-h-screen flex justify-center items-center'>
+      Homepage - Dana Indo Agri Website
+    </main>
+    </>
+  )
+}
